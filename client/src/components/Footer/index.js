5b2fec1ceeabfbb65f7725e5ee979319c8e86e52
Jacob Ashworth <jeashwor@gmail.com>
2020-11-06 23:03:30 -0500

    added footer component

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <hr />
+      <p className="text-center">
+        <i className="fab fa-github" /> JEA Designs - 2020
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;

d4930d18078a5d61dfbe3f1556550ecc347fb880
Jacob Ashworth <jeashwor@gmail.com>
2020-11-06 23:26:21 -0500

    updated clients sided package.json with proxy server info

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const path = require("path");
 const router = require("express").Router();
-const API = require("./api");
+const apiRoutes = require("./api");
 
 // Identify API routes
-router.use("/api", API);
+router.use("/api", apiRoutes);
 
 // Send to React App if no API routes are used
 router.use((req, res) =>
-    res.sendFile(path.join(__dirname, "../client/build/index.html"))
+  res.sendFile(path.join(__dirname, "../client/build/index.html"))
 );
 
 module.exports = router;

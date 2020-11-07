d2f78e9a02e8b59d2d06ee60ff1248f50ee0f0c9
Jacob Ashworth <jeashwor@gmail.com>
2020-11-07 09:19:19 -0500

    updated styling of NoMatch page and added duck gif

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -8,10 +8,10 @@ function NoMatch() {
       <Row>
         <Col size="md-12">
           <Jumbotron>
-            <h1 className="text-center">404 Page Not Found</h1>
-            <h1 className="text-center">
-              <span role="img" aria-label="Face With Rolling Eyes Emoji">
-                🙄
+            <h1 className="text-center">404 - Looking, but can't find it...</h1>
+            <h1 className="text-center mt-5">
+              <span role="img" aria-label="looking duck">
+                <iframe title="duck gif" src="https://giphy.com/embed/12zV7u6Bh0vHpu" width={200} height={200} frameBorder={0} className="giphy-embed" allowFullScreen></iframe>
               </span>
             </h1>
           </Jumbotron>

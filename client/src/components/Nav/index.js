8c235e8a8b473021990308d900205126b80396a9
Jacob Ashworth <jeashwor@gmail.com>
2020-11-07 10:40:57 -0500

    styling of elements with color scheme

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -32,7 +32,7 @@ class Nav extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light mb-2">
+      <nav className="navbar navbar-expand-lg navbar-light mb-2">
         <Link className="navbar-brand" to="/">
           Google Books
         </Link>
@@ -48,7 +48,7 @@ class Nav extends Component {
           <span className="navbar-toggler-icon" />
         </button>
         <div className={`${this.state.open ? "" : "collapse "}navbar-collapse`} id="navbarNav">
-          <ul className="navbar-nav">
+          <ul className="navbar-nav ml-auto">
             <li className="nav-item">
               <Link
                 onClick={this.toggleNav}
